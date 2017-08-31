8452f6454ee8a7c5632c9f747570b6535359754c
NGUYENVU\MyPC <vunguyen.catv@gmail.com>
2017-08-31 17:38:15 +0700

    no message

--- server.js
+++ server.js
@@ -10,36 +10,37 @@ import config from './api/v1/config'
 import v1 from './api/v1'
 
 // Port
-var port = process.env.PORT || '3000';
+var port = process.env.PORT || '3000'
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Connect database
 // mongoose.connect(config.DATABASE)
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect(config.DATABASE, { useMongoClient: true })
     .then(() => {
-        console.log('connected to mongodb');
+        console.log('connected to mongodb')
     })
     .catch((error) => {
-        console.log('error connecting to db:');
+        console.log('error connecting to db:')
     });
 
 // Use route
-app.use('/api/v1', v1);
+app.use('/api/v1', v1)
 
 // Create server
-var server = http.createServer(app);
+var server = http.createServer(app)
 
-server.listen(port);
+server.listen(port)
 server.on('error', () => {
     console.log('Server error!')
-});
+})
+
 server.on('listening', () => {
     console.log('Server running on: ' + port)
-});
+})
