4e6829daa56ab4ee9b965f7e6c21075481476a46
NGUYENVU\MyPC <vunguyen.catv@gmail.com>
2017-10-13 07:43:50 +0700

    ...

--- api/v1/config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    SECRET_TOKEN: 'vdbdfbdfb',
-    DATABASE: 'mongodb://duyenhaiit:Awsz2020@ds147872.mlab.com:47872/data_api'
-}
--- api/v1/controllers/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    auth: require('./auth'),
-    user: require('./user')
-}
